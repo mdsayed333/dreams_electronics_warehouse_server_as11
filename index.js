b9dfbdd0001fad08c4418a0b77cc29755f025d84
Sayed <mdsayedgv@gmail.com>
2022-05-04 04:55:42 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xe5ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const productsCollection = client.db("dreams-electronics").collection("products");
+        console.log('Mongo is Running');
+
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+    finally{
+        // client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
